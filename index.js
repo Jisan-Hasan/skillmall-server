0e7c9633edbfd5d2071236afb7df3b9738446d31
Jisan-Hasan <jisan.jh5@gmail.com>
2022-10-25 13:58:45 +0600

    browser cors added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+
+
 
 app.get("/", (req, res)=>{
     res.send("Server is responding");
 })
+
 
 app.listen(port, () => {
     console.log("Server is running on port: ", port);
